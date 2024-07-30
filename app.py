318008f13dab991d38721f4266ff75c8eb03300a
WhoamiTitle <137419138+WhoamiTitle@users.noreply.github.com>
2024-07-30 13:28:10 +0900

    FastAPI прил с эндпоинтами для установки и индексных страниц

---
+++ app.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+
+@app.head("/install")
+async def init_head():
+    pass
+
+
+@app.head("/index")
+async def index_head():
+    pass
+
+
+@app.post("/install", response_class=HTMLResponse)
+async def install_post():
+    return """
+    <head>
+        <script src="//api.bitrix24.com/api/v1/"></script>
+        <script>
+            BX24.init(function(){
+                BX24.installFinish();
+            });
+        </script>
+    </head>
+    <body>
+            installation has been finished.
+    </body>
+    """
+
+
+
+@app.post("/index", response_class=HTMLResponse)
+async def index_post():
+    return """
+    <head>
+        <script src="//api.bitrix24.com/api/v1/"></script>
+        <script>
+            BX24.init(function(){
+                BX24.installFinish();
+            });
+        </script>
+    </head>
+    <body>
+            installation has been finished.
+    </body>
+    """
